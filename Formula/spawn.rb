ed345c92deff59094e240bf0156a1ce5105c5db8
Mark Mayo <mark@vmunix.com>
2026-02-14 10:18:39 -0800

    Add spawn formula

---
+++ Formula/spawn.rb
@@ -0,0 +1,19 @@
+class Spawn < Formula
+  desc "Sandboxed AI coding agents on macOS"
+  homepage "https://github.com/vmunix/spawn"
+  url "https://github.com/vmunix/spawn/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 ""
+  license "MIT"
+
+  depends_on :macos
+  depends_on xcode: ["16.0", :build]
+
+  def install
+    system "swift", "build", "-c", "release", "--disable-sandbox"
+    bin.install ".build/release/spawn"
+  end
+
+  test do
+    assert_match "Sandboxed AI coding agents", shell_output("#{bin}/spawn --help 2>&1", 0)
+  end
+end
