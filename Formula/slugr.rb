2bcd8e7e9aaa72f946ce20f7e51dbc5cd877e49b
Mark Mayo <mark@vmunix.com>
2026-02-28 21:06:40 +0000

    Update slugr formula to 0.1.2

--- Formula/slugr.rb
+++ Formula/slugr.rb
@@ -1,17 +1,17 @@
 class Slugr < Formula
   desc "Rename files and directories to clean, URL and shell friendly slugs"
   homepage "https://github.com/vmunix/slugr"
-  version "0.1.1"
+  version "0.1.2"
   license "BSD-3-Clause"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/vmunix/slugr/releases/download/v0.1.1/slugr-x86_64-apple-darwin.tar.gz"
-      sha256 "ad1c37ee53d80a996252d42267e7a1fe7de3984670f49c950faa581da971f0e1"
+      url "https://github.com/vmunix/slugr/releases/download/v0.1.2/slugr-x86_64-apple-darwin.tar.gz"
+      sha256 "3440c9fd0008f03412de19b528cb590e69b5eea5aa2071f49e336487c0381828"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/vmunix/slugr/releases/download/v0.1.1/slugr-aarch64-apple-darwin.tar.gz"
-      sha256 "09a4d44fd418206b30b7d7b0ab4880992caf73502169a3f52bbc68f1baf932f7"
+      url "https://github.com/vmunix/slugr/releases/download/v0.1.2/slugr-aarch64-apple-darwin.tar.gz"
+      sha256 "9a4e6ad817adf62bde366328f78f4bc2bb271c3e9a495309395833187c82f1e8"
     end
   end
 
